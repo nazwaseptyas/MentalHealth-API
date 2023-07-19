c1920e69129966334eb10764071b11490b6013b4
baploarya <aryawardhana7831@gmail.com>
2023-07-19 18:41:16 +0700

    get data konsultasi

--- controllers/KonsultasiController.js
+++ controllers/KonsultasiController.js
@@ -2,6 +2,14 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+export const getDataKonsultasi = async (req, res) => {
+  try {
+    const response = await prisma.konsultasi.findMany();
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
 export const createKonsultasi = async (req, res) => {
   const { nama, email, alamat, nohp, keluhan, tanggal } = req.body;
   try {
